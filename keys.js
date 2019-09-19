c5d3d0bfd28f13b538376e51cb0b59c77fd5c423
rachel doran <rmitchell0925@gmail.com>
2019-09-19 19:01:11 -0400

    finally after asking twitter like a lot, got my API key

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.twitterKeys = {
+    consumer_key: 'Mn0HPTVWRPwKkqc8q3B0a7326',
+    consumer_secret: 'iwHTS4noovq0ONKyDMQJWoBoj6eh4Wgt4YsNdTfZU6tJffxhAE',
+    access_token_key: '906542030763028480-YBadJlItvLlGOFwphkM4K3uYmn7lSBD',
+    access_token_secret: 'dHit4BZhEu8mAiIZp84YheeZc1u9Rq6AoCoJwkUWax8lL',
+  }
--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var params = {
+    screen_name: 'rachmitch_'
+} && {
+    count: 20
+};
