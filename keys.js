f6a433c0838678d3733c655796411283fa3942ca
rachel doran <rmitchell0925@gmail.com>
2019-09-19 19:12:29 -0400

    ya know just fixing errors

--- keys.js
+++ keys.js
@@ -1,10 +1,5 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
-
 exports.twitterKeys = {
     consumer_key: 'Mn0HPTVWRPwKkqc8q3B0a7326',
     consumer_secret: 'iwHTS4noovq0ONKyDMQJWoBoj6eh4Wgt4YsNdTfZU6tJffxhAE',
--- liri.js
+++ liri.js
@@ -1,9 +1,49 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+
 
 var params = {
     screen_name: 'rachmitch_'
 } && {
     count: 20
 };
+
+liri
+.prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+      type: "password",
+      message: "Set your password",
+      name: "password"
+    },
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Which Pokemon do you choose?",
+      choices: ["Bulbasaur", "Squirtle", "Charmander"],
+      name: "pokemon"
+    },
+    // Here we ask the user to confirm.
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }
+  ])
+  .then(function(liriResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (liriResponse.confirm) {
+      console.log("\nWelcome " + liriResponse.username);
+      console.log("Your " + liriResponse + " is ready for battle!\n");
+    }
+    else {
+      console.log("\nThat's okay " + liriResponse.username + ", come again when you are more sure.\n");
+    }
+  });
